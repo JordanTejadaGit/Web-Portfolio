06304cb06ad0b4fc97f48b6bdbfd6a7fc711cf18
Jordan Tejada <jordan.tejada5@gmail.com>
2022-01-25 15:45:21 -0700

    l

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -2,36 +2,36 @@ export default [
     {
         name: 'initial',
         type: 'texture',
-        path: 'logo/initial.png'
+        path: '/logo/initial.png'
     },
     {
         name: 'skillsLogo',
         type: 'texture',
-        path: 'logo/skilllogo.png'
+        path: '/logo/skilllogo.png'
     },
     {
         name: 'htmlLogo',
         type: 'texture',
-        path: 'logo/htmllogo.png'
+        path: '/logo/htmllogo.png'
     },
     {
         name: 'cssLogo',
         type: 'texture',
-        path: 'logo/csslogo.png'
+        path: '/logo/csslogo.png'
     },
     {
         name: 'project1',
         type: 'texture',
-        path: 'pictures/project_portfolio.png'
+        path: '/pictures/project_portfolio.png'
     },
     {
         name: 'project2',
         type: 'texture',
-        path: 'pictures/project2_portfolio.png'
+        path: '/pictures/project2_portfolio.png'
     },
     {
         name: 'project3',
         type: 'texture',
-        path: 'pictures/project3_portfolio.png'
+        path: '/pictures/project3_portfolio.png'
     }
 ]
